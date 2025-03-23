d1da6a11fef6f1f2355fb2dea71574757c59c2cc
hkiame <henrykiame@gmail.com>
2025-03-23 10:21:57 +0300

    adds redux provider to the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AppRoutes from "@/routes";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <AppRoutes />
-    </>
+    </Provider>
   );
 }
 
